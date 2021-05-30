3a16601e35d82b332e27226c4d45ae97ba3951eb
Poom Yimyuean <lebrancconvas@gmail.com>
2021-05-30 19:08:03 +0700

    :sparkles: Add Register Page.

---
+++ client/src/views/Register.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <form>
+           <h2>Register</h2>
+           <div class="input-group mb-3">
+               <div class="input-group-prepend">
+                   <span class="input-group-prepend" id="basic-addon1">
+                        Username
+                   </span> 
+                   <input type="text" class="form-control" placeholder="Enter Your Username" aria-label="Username" aria-describedby="basic-addon1">
+               </div> 
+           </div>
+           <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-prepend" id="basic-addon1">
+                        Password
+                    </span>
+                    <input type="password" class="form-control" placeholder="Enter Your Password" aria-label="Password" aria-describedby="basic-addon1">
+                </div>
+           </div>
+           <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-prepend">
+                        Confirm Password 
+                    </span>
+                    <input type="password" class="form-control" placeholder="Confirm Your Password" aria-label="Password" aria-describedby="basic-addon1">
+                </div>
+           </div>     
+        </form> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Register"
+}
+</script>
+
+<style scoped>
+
+</style>
