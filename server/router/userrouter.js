30a18d53d1273077df642f24edcab51d4eb95b12
Poom Yimyuean <lebrancconvas@gmail.com>
2021-05-31 18:44:52 +0700

    :sparkles: Add GET Method in User Router.

--- server/router/userrouter.js
+++ server/router/userrouter.js
@@ -1,3 +1,11 @@
 let express = require('express')
 let router = expree.Router()
 let Users = require('../model/usermodel')
+
+//GET All
+router.get('/', (req, res) => {
+    Food.find().exec((err, data) => {
+        if (err) return res.status(400).send(err)
+        res.status(200).send(data)
+    })
+})
